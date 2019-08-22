453a447acdc26bc220e1337e07aa8c18cbb4ad15
Mike Lewis <mike@mplew.is>
2019-08-22 16:09:13 -0400

    Reformat and add margins when rendering

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('file:///' + process.cwd() + '/resume.html', {waitUntil: 'networkidle2'});
-  await page.pdf({path: 'resume.pdf'});
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto('file:///' + process.cwd() + '/resume.html', {
+		waitUntil: 'networkidle2'
+	});
+	await page.pdf({
+		path: 'resume.pdf',
+		margin: {
+			top: "0.25in",
+			right: "0.25in",
+			bottom: "0.25in",
+			left: "0.25in",
+		}
+	});
 
-  await browser.close();
+	await browser.close();
 })();
